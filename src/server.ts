4838c4472f68045c301de9ba8969725929d8c94a
Mayara <ads.nangela@gmail.com>
2021-06-25 12:38:38 -0300

    Inclusão regra Access-Control-Allow-Origin porta 4200 onde está rodando o Front Angular

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,25 @@ import { router } from "./config/routes";
 const app = express();
 const db = mongoose;
 
+
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  next();
+});
+
 app.use(express.json());
 app.use(router);
+
 
 app.listen(1234, () => {
   console.log("O servidor está rodando...");
